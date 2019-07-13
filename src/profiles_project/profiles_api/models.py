d572a47ccfe5c9cba1670779d61372db5d8353b3
Ahmed Shahwan <ahmed@shahwan.me>
2019-07-13 04:02:05 +0200

    Added a custom user profile model, a manager, and the db migrations

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model."""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile object."""
+
+        if not email:
+            raise ValueError("User must have an email address.")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details.
+        This method is used when you run `./manage.py createsuperuser` command"""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represents a "user profile" inside our system."""
+
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=254)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    # the default login field
+    USERNAME_FIELD = "email"
+    # the requried fields besides the USERNAME_FIELD
+    REQUIRED_FIELDS = ["name"]
+
+    def get_full_name(self):
+        """Use to get a user's full name."""
+
+        return self.name
+
+    def get_short_name(self):
+        """Use to get a user's short name."""
+
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to a string."""
+        return self.email
--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Custom user model to be used
+AUTH_USER_MODEL = "profiles_api.UserProfile"
