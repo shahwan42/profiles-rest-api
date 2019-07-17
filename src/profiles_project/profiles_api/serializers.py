29317c563a37dfb633b7bb033fb0f95e650f86d1
Ahmed Shahwan <ahmed@shahwan.me>
2019-07-17 15:09:13 +0200

    profile listing and updating feature

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -1,7 +1,34 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView."""
 
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for our user profile objects."""
+
+    class Meta:
+        # model to be used by this serializer
+        model = models.UserProfile
+        # the fields this serializer will manage
+        fields = ("id", "email", "name", "password")
+        # make password field write-only, security-wise: password must not be read
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        """Overrides model manager's create method. Creates and returns a new user."""
+
+        # create new user object using the validated data
+        user = models.UserProfile(
+            email=validated_data.get("email"),
+            name=validated_data.get("name")
+        )
+        # use the spaciale set_password to encrypt the password
+        user.set_password(validated_data.get("password"))
+        user.save()
+
+        return user
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from . import serializers
+from . import serializers, models
 
 
 class HelloApiView(APIView):
@@ -96,3 +96,10 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles removing an object."""
 
         return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading, and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
